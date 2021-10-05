1a5a26b3ab50ba62c19350928d8efdef6146f3f0
philippe <francois-philippe.ossim-belias@polymtl.ca>
2021-10-05 17:34:34 -0400

    commit log extraction done
    Signed-off-by: philippe <francois-philippe.ossim-belias@polymtl.ca>

---
+++ scripts/extract.py
@@ -0,0 +1,45 @@
+""" This code is used to extract information from a git repository
+    information are : commit log, list of all modified file, the complete version
+    history of all identified test files and their production files.
+    by Ossim Belias François Philippe"""
+
+from pydriller.git import Git
+from pydriller.domain.commit import ModifiedFile, ModificationType
+from pydriller.repository import Repository
+import pandas as pd
+
+
+def extractCommitLog(repository_name):
+    """Extract the entire commit log of a repository"""
+    gr = Git(repository_name)
+    commits = gr.get_list_commits()
+    for commit in commits:
+        print("commit {}".format(commit.hash),
+              "Author: {}".format(commit.author),
+              "Date: {}".format(commit.author_date),
+              "Message: {}".format(commit.msg))
+
+
+def extractModifiedFileList(repository_name):
+    """Extract list of modified file for a repository"""
+    for commit in Repository(repository_name).traverse_commits():
+        for file in commit.modified_files:
+            print("commit {}".format(commit), "modified file {}".format(file.filename))
+
+
+def main():
+    # list of all projects
+    filename = "./combined_projects.csv"
+
+    df = pd.read_csv(filename)
+    repositories_list = list(df["name"])
+
+    for repo in repositories_list:
+        extractCommitLog(repo)
+        break
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
